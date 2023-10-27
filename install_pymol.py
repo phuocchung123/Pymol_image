c78dab7e45afd5b06c3e9bc5d839a8aae69269b7
phuocchung123 <88260422+phuocchung123@users.noreply.github.com>
2023-10-27 23:30:16 +0700

    Add files via upload

---
+++ install_pymol.py
@@ -0,0 +1,23 @@
+from IPython.utils import io
+import tqdm.notebook
+import os
+class install_pymol:
+    def __init__(self):
+        pass
+    def install_Pymol_source(self):
+        total = 100
+        with tqdm.notebook.tqdm(total=total) as pbar:
+            with io.capture_output() as captured:
+                !pip install -q condacolab
+                import condacolab
+                condacolab.install()
+                pbar.update(10)
+        
+                import sys
+                sys.path.append('/usr/local/lib/python3.7/site-packages/')
+                pbar.update(20)
+        
+                # Install PyMOL
+                %shell mamba install pymol-open-source --yes
+        
+                pbar.update(100)
